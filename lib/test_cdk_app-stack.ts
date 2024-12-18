2d1e6f5075a74cbb0bb571f0cf143b93a5beac4a
Phil Stenning <philstenning@hotmail.com>
2024-12-18 17:09:46 +0000

    inital commit

--- lib/test_cdk_app-stack.ts
+++ lib/test_cdk_app-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +9,8 @@ export class TestCdkAppStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'TestCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'TestCdkAppQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
   }
 }
